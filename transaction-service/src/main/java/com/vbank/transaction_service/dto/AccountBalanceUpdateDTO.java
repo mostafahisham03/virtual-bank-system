21e02bd83d8ee405dee720b183cfcefa38271af4
Mostafa Hisham <52044516+mostafahisham03@users.noreply.github.com>
2025-07-29 21:11:36 +0300

    minor fix

--- transaction-service/src/main/java/com/vbank/transaction_service/dto/AccountBalanceUpdateDTO.java
+++ transaction-service/src/main/java/com/vbank/transaction_service/dto/AccountBalanceUpdateDTO.java
@@ -7,13 +7,22 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
 public class AccountBalanceUpdateDTO {
+    @NotNull
+    @NotBlank(message = "From account ID cannot be null")
     private UUID fromAccountId;
+    @NotNull
+    @NotBlank(message = "To account ID cannot be null")
     private UUID toAccountId;
+    @NotNull
+    @NotBlank(message = "Amount cannot be null")
     private BigDecimal Amount;
 
 }
